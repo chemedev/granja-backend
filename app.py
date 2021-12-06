e996f77f83be0bae6afd0b47bd3d202d1d4a8fc6
Juan E. Chemello <juan.chemello@bue.edu.ar>
2021-12-06 18:09:38 -0300

    Se agrega index y /products

--- app.py
+++ app.py
@@ -19,3 +19,27 @@ except OperationalError as error:
     print(msg)
     print("Verificar si la DB está ejecutándose")
     print("_" * 100)
+
+
+@app.route('/')
+def index():
+    return '<h2>Esto es una API.</h2> \
+            <p><a href="/products">/products</a></p> \
+            <p><a href="/orders">/orders</a></p> \
+            '
+
+
+@app.route('/products')
+def products():
+    sql = 'select * from products where is_deleted = 0;'
+    try:
+        cursor.execute(sql)
+        response = cursor.fetchall()
+    except ProgrammingError as error:
+        code, msg = error.args
+        response = (code, msg)
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
