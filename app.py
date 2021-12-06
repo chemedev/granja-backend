cc382d749c6418a475ec1107f5730136688afd36
Juan E. Chemello <juan.chemello@bue.edu.ar>
2021-12-06 18:08:56 -0300

    Conexion a MySQL

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flaskext.mysql import MySQL
+from pymysql import OperationalError
+from pymysql.cursors import DictCursor
+from pymysql.err import ProgrammingError
+
+app = Flask(__name__)
+mysql = MySQL(app)
+
+app.config['MYSQL_DATABASE_USER'] = 'admin'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'granja'
+
+try:
+    cursor = mysql.connect().cursor(cursor=DictCursor)
+except OperationalError as error:
+    code, msg = error.args
+    print("_" * 100)
+    print(msg)
+    print("Verificar si la DB está ejecutándose")
+    print("_" * 100)
